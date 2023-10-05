3df5dc94afdfeb079115c9e122215bd467fa3590
xpandreifilippi <128719165+xpandreifilippi@users.noreply.github.com>
2023-10-05 09:08:46 -0300

    05 10

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 alert ('seja bem vindo');
+
+
+document .querySelector(.tecla_pom) . onclick= tocaSomPom;
+function tocaSomPom(){document. queryselector}(#som tecla_pom). play()
+
+document .querySelector(.tecla_tim ) . onclick= toca_Som;_tim
+function tocaSomPom(){document. queryselector}(#som tecla_tim). play()
+}
+document .querySelector(.tecla_PUFF) . onclick= toca_Som;_PUFF
+function tocaSomPom(){document. queryselector}(#som tecla_PUFF). play()
+
+document .querySelector(.tecla_SPLASH ) . onclick= toca_Som;_SPLASH
+function tocaSomPom(){document. queryselector}(#som tecla_SPLASH). play()
+
+document .querySelector(.tecla_toim ) . onclick= toca_Som;_toim
+function tocaSomPom(){document. queryselector}(#som teclatoim). play()
+
+document .querySelector(.tecla_psh ) . onclick= toca_Som;_psh
+function tocaSomPom(){document. queryselector}(#som tecla_psh). play()
+
+document .querySelector(.tecla_tic ) . onclick= toca_Som;_tic
+function tocaSomPom(){document. queryselector}(#som tecla_tic). play()
+
+document .querySelector(.tecla_tom ) . onclick= toca_Som;_tom
+function tocaSomPom(){document. queryselector}(#som tecla_tom). play()
+
+document .querySelector(.tecla_clap ) . onclick= toca_Som;_clap
+function tocaSomPom(){document. queryselector}(#som tecla_clap). play()
+
+
+
